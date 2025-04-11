022862f2ad781befa4b99b274ea5521274a9d636
Gab-23 <gabrieleoliveto26@gmail.com>
2025-04-11 09:15:28 +0200

    Update Pipelines and Multiclass Classification

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+declare -i PORT_NUMBER=$1
+
+
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate GAB_ENV
-jupyter notebook --no-browser --port=9999
+jupyter notebook --no-browser --port=$PORT_NUMBER
 
 
 
